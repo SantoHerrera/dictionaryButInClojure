7d552a4c1d36c2ba3ac876795d2d03a9df1b567d
Rafael Herrera <santoElias65537@gmail.com>
2020-06-27 00:44:47 -0600

    gets input call other function with string

--- src/dictionarybutinclojure/core.clj
+++ src/dictionarybutinclojure/core.clj
@@ -5,3 +5,23 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn printInput
+  [idk] (println idk "nigga we made it"))
+
+;(defn x (read-line))
+
+(do (print "What's your name? ")
+           (flush)
+           (printInput (str (read-line))))
+
+;(do (print "What's your name? ")
+;           (flush)
+;           (read-line))
+;
+;this works
+;(do (print "What's your name? ") 
+;           (flush) 
+;
+;           (read-line))
+;(flush (read-line))

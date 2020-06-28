104ac938368e7b43a4a44eb0e5f9945b593c2d08
Rafael Herrera <santoElias65537@gmail.com>
2020-06-28 00:01:32 -0600

    got it to call function in another file. struggled with a lot. fml

--- src/dictionarybutinclojure/core.clj
+++ src/dictionarybutinclojure/core.clj
@@ -1,27 +1,33 @@
 (ns dictionarybutinclojure.core
-  (:gen-class))
+  (:gen-class)
+  (:require [dictionarybutinclojure.mainMain :as main]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
-(defn printInput
-  [idk] (println idk "nigga we made it"))
-
+;(defn printInput
+ ; [idk] (println idk "nigga we made it"))
+;(printInput "yeah")
 ;(defn x (read-line))
 
+(defn -main [& args]
+ (main/nameOfFunctionHere "your couch nigga"))
+ ;(mainMain/test))
+
 (do (print "What's your name? ")
-           (flush)
-           (printInput (str (read-line))))
+ (flush)
+  (read-line))
+
 
 ;(do (print "What's your name? ")
 ;           (flush)
 ;           (read-line))
 ;
 ;this works
-;(do (print "What's your name? ") 
-;           (flush) 
+;(do (print "What's your name? ")
+;           (flush)
 ;
 ;           (read-line))
 ;(flush (read-line))
---
+++ src/dictionarybutinclojure/mainMain.clj
@@ -0,0 +1,12 @@
+(ns dictionarybutinclojure.mainMain
+ (:gen-class))
+
+
+;(defn getInput []
+; (do (print "What's your name? ")
+; (flush)
+;  (printInput (str (read-line)))))
+
+(defn nameOfFunctionHere 
+ [here]
+  (println "fuck" here))

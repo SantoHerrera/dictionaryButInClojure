463364b17058214496f7cef1e9009b8fe8db3e73
Rafael Herrera <santoElias65537@gmail.com>
2020-06-28 23:37:30 -0600

    got it to get json and make it into objects and such, now i gotta destructure returned json and print def. sounds easy, it always does

--- src/dictionarybutinclojure/core.clj
+++ src/dictionarybutinclojure/core.clj
@@ -2,16 +2,5 @@
   (:gen-class)
   (:require [dictionarybutinclojure.mainMain :as main]))
 
-
-;(:require [cheshire.core :refer :all])
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
 (defn -main [& args]
  (main/getInput))
-
-;(def work {:fuck "shit"})
-;(generate-string work)
--- src/dictionarybutinclojure/mainMain.clj
+++ src/dictionarybutinclojure/mainMain.clj
@@ -1,20 +1,17 @@
 (ns dictionarybutinclojure.mainMain
- (:gen-class))
+ (:gen-class)
+ (:require [cheshire.core :refer :all]))
 
 ;(:require [:use cheshire.core])
 
 
 (defn getNewUrl
  [word]
- (println (str "https://dictionaryapi.com/api/v3/references/" "spanish" "/json/" word "?key=" "226a8d2c-2502-4efd-ac72-5a26eb317695")))
+ (str "https://dictionaryapi.com/api/v3/references/" "spanish" "/json/" word "?key=" "226a8d2c-2502-4efd-ac72-5a26eb317695"))
 
 (defn getInput []
- (do (print "Enter word to find definition")
+ (do (print "Enter word to find definition ------")
   (flush)
   (getNewUrl (str (read-line)))))
 
-(defn randomT [none] (println (str none)))
-
-;((def willIt {:fuck "yeah"}))
-
-(generate-string willIt)
+;(parse-string (slurp (getNewUrl "platano")))

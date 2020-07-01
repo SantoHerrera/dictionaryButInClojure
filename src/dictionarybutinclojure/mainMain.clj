ee7f66d7e59ff63eecd044d44a1c74064e3b22c4
Rafael Herrera <santoElias65537@gmail.com>
2020-06-30 23:32:59 -0600

    it works now, need to refactor

--- src/dictionarybutinclojure/mainMain.clj
+++ src/dictionarybutinclojure/mainMain.clj
@@ -1,17 +1,19 @@
 (ns dictionarybutinclojure.mainMain
  (:gen-class)
- (:require [cheshire.core :refer :all]))
+ (:require [cheshire.core :refer :all])
 
-;(:require [:use cheshire.core])
+
+
 
 
 (defn getNewUrl
- [word]
- (str "https://dictionaryapi.com/api/v3/references/" "spanish" "/json/" word "?key=" "226a8d2c-2502-4efd-ac72-5a26eb317695"))
+  [word]
+  (str "https://dictionaryapi.com/api/v3/references/" "spanish" "/json/" word "?key=" "226a8d2c-2502-4efd-ac72-5a26eb317695"))
+
+(defn getJson [word] (parse-string (slurp (getNewUrl word))))
+
 
 (defn getInput []
- (do (print "Enter word to find definition ------")
+ (do (println "Enter word to find definition ->")
   (flush)
-  (getNewUrl (str (read-line)))))
-
-;(parse-string (slurp (getNewUrl "platano")))
+  (mapcat #(% "shortdef") (getJson (str (read-line))))))

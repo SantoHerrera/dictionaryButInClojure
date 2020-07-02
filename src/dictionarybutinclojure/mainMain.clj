4090d31bc9275f85c63e04251a37f42be0c3ba68
Rafael Herrera <santoElias65537@gmail.com>
2020-07-02 00:12:38 -0600

    works in terminal now

--- src/dictionarybutinclojure/mainMain.clj
+++ src/dictionarybutinclojure/mainMain.clj
@@ -1,19 +1,11 @@
 (ns dictionarybutinclojure.mainMain
  (:gen-class)
- (:require [cheshire.core :refer :all])
-
-
-
-
-
+ (:require [cheshire.core :refer :all]))
 (defn getNewUrl
   [word]
   (str "https://dictionaryapi.com/api/v3/references/" "spanish" "/json/" word "?key=" "226a8d2c-2502-4efd-ac72-5a26eb317695"))
-
 (defn getJson [word] (parse-string (slurp (getNewUrl word))))
-
-
 (defn getInput []
  (do (println "Enter word to find definition ->")
   (flush)
-  (mapcat #(% "shortdef") (getJson (str (read-line))))))
+  (println (mapcat #(% "shortdef") (getJson (str (read-line)))))))

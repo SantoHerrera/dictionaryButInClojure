0382321c10fa703cceb8bb5a728dfe93adf0f6d9
Rafael Herrera <santoElias65537@gmail.com>
2020-07-17 21:29:06 -0600

    reworked some stuff, idk if its for the better. Longer but should make more readable long term???

--- src/dictionarybutinclojure/mainMain.clj
+++ src/dictionarybutinclojure/mainMain.clj
@@ -1,11 +1,36 @@
 (ns dictionarybutinclojure.mainMain
  (:gen-class)
  (:require [cheshire.core :refer :all]))
+
+(declare getInput)
+
 (defn getNewUrl
   [word]
   (str "https://dictionaryapi.com/api/v3/references/" "spanish" "/json/" word "?key=" "226a8d2c-2502-4efd-ac72-5a26eb317695"))
+
 (defn getJson [word] (parse-string (slurp (getNewUrl word))))
+
+
+(defn bulletPoint
+  [word]
+  (println (str "> " word)))
+
+
+
+(def nothingEnteredPrompt "please enter something before hitting enter :)")
+
+(defn setUp ([] (println nothingEnteredPrompt) (getInput))
+  ([wordEntered] (let [wordJson (parse-string (slurp (getNewUrl wordEntered)))]
+                  (run! println (mapcat #(% "shortdef") wordJson)))))
+
 (defn getInput []
- (do (println "Enter word to find definition ->")
-  (flush)
-  (println (mapcat #(% "shortdef") (getJson (str (read-line)))))))
+ (println "Enter word to find definition ->")
+ (flush)
+ (let [wordEntered (read-line)]
+  (if (clojure.string/blank? wordEntered)
+    (setUp)
+    (setUp wordEntered))))
+
+(defn whatToDo [word] (println (str word)))
+
+(defn input [] (println "worked"))
